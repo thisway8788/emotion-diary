bd8cecd62e8755830cd0f55371bbb1c63921b777
Jeung hak Ham <thisway8788@gmail.com>
2022-11-03 01:50:02 -0400

    font added
    google fonts

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,37 @@
+import { useSearchParams, useNavigate } from 'react-router-dom';
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  console.log('id: ', id);
+
+  const mode = searchParams.get('mode');
+  console.log('mode: ', mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>This is Edit</p>
+      <button onClick={() => setSearchParams({ who: 'jeff' })}>
+        QS Change
+      </button>
+
+      <button
+        onClick={() => {
+          navigate('/home');
+        }}
+      >
+        Go To Home
+      </button>
+
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
